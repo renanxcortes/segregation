ac52c85094bb010d0fe6bcbbc7c9707fa7319d5c
renanxcortes <renanxcortes@gmail.com>
2019-06-18 17:05:49 -0700

    change library to rtree

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['libspatialindex_c']
+MOCK_MODULES = ['rtree']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
