f0ab6f5f65975675ce7f3562c35533d0f77df672
renanxcortes <renanxcortes@gmail.com>
2019-06-18 17:17:56 -0700

    add numpydoc to mock

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['rtree', 'sphinxcontrib.bibtex']
+MOCK_MODULES = ['rtree', 'sphinxcontrib.bibtex' 'numpydoc']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
