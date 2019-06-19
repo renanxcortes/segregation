7e422f8356dc8f9b73af5ca79ea6c2399fc3beab
renanxcortes <renanxcortes@gmail.com>
2019-06-18 17:16:01 -0700

    put sphinx bibtex in the mock

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['rtree']
+MOCK_MODULES = ['rtree', 'sphinxcontrib.bibtex']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
