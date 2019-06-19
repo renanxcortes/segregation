10a85ba996afd46c833e7c1c8b535d76cc2690cf
renanxcortes <renanxcortes@gmail.com>
2019-06-18 21:27:54 -0700

    add bibtex after mock conf

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['rtree', 'sphinxcontrib.bibtex', 'numpydoc']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
+import sphinxcontrib.bibtex
 
 sys.path.insert(0, os.path.abspath('../../'))
 
